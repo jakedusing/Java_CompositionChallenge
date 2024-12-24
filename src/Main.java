daab293a1ed933256bfddae84b1d6f27631408ec
Jake Dusing <dusingjake@gmail.com>
2024-12-23 20:40:22 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SmartKitchen kitchen = new SmartKitchen();
+
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
+
+        /*kitchen.getDishWasher().setHasWorkToDo(true);
+        kitchen.getIceBox().setHasWorkToDo(true);
+        kitchen.getBrewMaster().setHasWorkToDo(true);
+
+        kitchen.getDishWasher().doDishes();
+        kitchen.getIceBox().orderFood();
+        kitchen.getBrewMaster().brewCoffee();*/
+    }
+}
